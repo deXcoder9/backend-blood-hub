fc0c5288aed66ec61814c5ba439e32983cee957e
deXcoder9_ <dhrubothejuniorprogrammer@gmail.com>
2024-06-05 03:22:31 +0600

    adminPanel [all users complete]

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -32,7 +32,13 @@ async function run() {
         // users related api
         app.post('/users', async(req, res) => {
             const user = req.body;
+            const query = {email : user.email}
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser) {
+              return res.send({message: "User already exists", insertedId: null})
+            }
             const result = await usersCollection.insertOne(user)
+            // const allUsers =  await usersCollection.find().toArray()
             res.send(result);
         })
 
@@ -40,6 +46,44 @@ async function run() {
           const result =  await usersCollection.find().toArray()
           res.send(result);
         })
+
+        app.patch("/users/makeadmin/:id", async(req, res) => {
+          const id = req.params.id;
+          const filter = {_id : new ObjectId(id)}
+          const updateDoc = {
+            $set : {role : "admin"}
+          }
+          const result = await usersCollection.updateOne(filter, updateDoc)
+          res.send(result);
+        })
+        app.patch("/users/makevolunteer/:id", async(req, res) => {
+          const id = req.params.id;
+          const filter = {_id : new ObjectId(id)}
+          const updateDoc = {
+            $set : {role : "volunteer"}
+          }
+          const result = await usersCollection.updateOne(filter, updateDoc)
+          res.send(result);
+        })
+        app.patch("/users/block/:id", async(req, res) => {
+          const id = req.params.id;
+          const filter = {_id : new ObjectId(id)}
+          const updateDoc = {
+            $set : {status : "block"}
+          }
+          const result = await usersCollection.updateOne(filter, updateDoc)
+          res.send(result);
+        })
+        app.patch("/users/unblock/:id", async(req, res) => {
+          const id = req.params.id;
+          const filter = {_id : new ObjectId(id)}
+          const updateDoc = {
+            $set : {status : "active"}
+          }
+          const result = await usersCollection.updateOne(filter, updateDoc)
+          res.send(result);
+        })
+
        
 
         // Profile related api
