9397193343805b295018f77799c417a59cd12b6b
deXcoder9_ <dhrubothejuniorprogrammer@gmail.com>
2024-06-20 15:51:54 +0600

    all complete

--- index.js
+++ index.js
@@ -315,6 +315,12 @@ async function run() {
       console.log(result)
     });
 
+    app.get("/getblogdetailsbyid/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(filter);
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
